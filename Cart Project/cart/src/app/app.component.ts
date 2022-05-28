4acfa99f56cb884b24b1f1a412ce42e5d848b486
HabibIbrahimov <habibyi@code.edu.az>
2022-05-28 20:29:44 +0400

    feat:Prepared array containing objects

--- Cart Project/cart/src/app/app.component.ts
+++ Cart Project/cart/src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cart';
+  posts=[
+    {
+      title:'Dağ Bisikleti',
+      imageUrl:'assets/biking.jpeg',
+      username:'cbdag',
+      content:'Çok hızlı sürdüm'
+    },
+    {
+     title: 'Tırmanış',
+     imageUrl: 'assets/mountain.jpeg',
+     username: 'cbuludag',
+     content: 'Bugünde iyi tırmandım'
+   },
+   {
+     title: 'Doğa Yürüyüşü',
+     imageUrl: 'assets/tree.jpeg',
+     username: 'cbdoga',
+     content: 'Hadi biraz Maçka Parkında turlayalım'
+   },
+ 
+  ]
 }
