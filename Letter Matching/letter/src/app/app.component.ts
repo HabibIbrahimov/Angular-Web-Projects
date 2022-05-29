c7bc706c73c7dc7679813b414f66853472c1ab13
HabibIbrahimov <habibyi@code.edu.az>
2022-05-29 17:45:44 +0400

    feat:added split method and class binding

--- Letter Matching/letter/src/app/app.component.ts
+++ Letter Matching/letter/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {lorem} from 'faker'
+import {lorem} from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,27 @@ import {lorem} from 'faker'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  randomText=lorem.sentence();
-  title = 'letter';
+
+  randomText = lorem.sentence();
+  enteredText = '';
+  can="incorrect";
+
+
+  getInputValue(value: string)
+  {
+    console.log(this.randomText.split(''));
+    this.enteredText=value;
+
+  }
+
+  compare(randomLetter: string , enteredLetter: string)
+  {
+   if(!enteredLetter)
+   {
+     return 'pending';
+   }
+
+   return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
+
 }
