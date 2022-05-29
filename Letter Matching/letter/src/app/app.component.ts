72c708558f956af98f4a9d76e338f5fa106d673c
HabibIbrahimov <habibyi@code.edu.az>
2022-05-29 15:08:56 +0400

    feat:added faker

--- Letter Matching/letter/src/app/app.component.ts
+++ Letter Matching/letter/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {lorem} from 'faker'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  randomText=lorem.sentence();
   title = 'letter';
 }
