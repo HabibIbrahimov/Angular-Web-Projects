7764a8e9bf9e5ebf8aab45825a585140cbdb32ef
HabibIbrahimov <habibyi@code.edu.az>
2022-05-30 13:02:50 +0400

    feat: added titlecase pipe

--- Angular Pipes/pipes/src/app/app.component.ts
+++ Angular Pipes/pipes/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+  name = '';
+
+
+  onNameChange(value: string)
+  {
+    this.name =value;
+  }
 }
