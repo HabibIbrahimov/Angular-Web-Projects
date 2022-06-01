f66412877248db2206b303de30c97ab816e7cdf7
HabibIbrahimov <habibyi@code.edu.az>
2022-06-01 14:01:41 +0400

    feat:pipe arguments

--- Angular Pipes/pipes/src/app/convert.pipe.ts
+++ Angular Pipes/pipes/src/app/convert.pipe.ts
@@ -5,8 +5,22 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvertPipe implements PipeTransform {
 
-  transform(value: any, ...args: unknown[]): unknown {
-    return value *1.60934;
+  transform(value: any, type:string): unknown {
+
+    console.log(type);
+
+    switch(type)
+    {
+      case 'km':
+        return  value * 1.60934;
+      case 'm':
+        return  value * 1.60934*1000;
+      case 'cm':
+        return  value * 1.60934*1000*1000;
+      default:
+        throw new Error('Böyle bir değer yok')
+    }
+
   }
 
 }
