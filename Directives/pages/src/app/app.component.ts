fee45d8e4692df35f9320aaa17e2a290abb6efc1
HabibIbrahimov <habibyi@code.edu.az>
2022-06-02 13:48:20 +0400

    feat:added bootstrap pagination and ngClass

--- Directives/pages/src/app/app.component.ts
+++ Directives/pages/src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  currentPage=0;
+ images=[
+   {
+     title:'Deniz Kum Gunes',
+     url:'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+   },
+   {
+    title:'Palm 1',
+    url:'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+   },
+   {
+    title:'Palm 2',
+    url:'https://images.unsplash.com/photo-1506953823976-52e1fdc0149a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+   },
+   {
+    title:'Palm 3',
+    url:'https://images.unsplash.com/photo-1520454974749-611b7248ffdb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+   },
+ ]
 }
