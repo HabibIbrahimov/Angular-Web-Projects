cc8633c3abbc0beea147421f3d0f24382bfaed4f
HabibIbrahimov <habibyi@code.edu.az>
2022-06-06 20:40:09 +0400

    feat:Table Data

--- Module/modcom/src/app/tables/table/table.component.ts
+++ Module/modcom/src/app/tables/table/table.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+  data = [
+    { name: 'Can', age: '29', job: 'Endüstri Mühendisi', employed: 'Hayır' },
+    { name: 'Osman', age: '30', job: 'Yazılım Mühendisi', employed: 'Hayır' },
+    { name: 'Elif', age: '27', job: 'İşletme Mezunu', employed: 'Evet' }
+  ];
+  headers = [
+    { key: 'employed', label: 'Aldığı ücretten memnun mu?' },
+    { key: 'name', label: 'Ad Soyad' },
+    { key: 'age', label: 'Yaş' },
+    { key: 'job', label: 'Meslek' }
+  ];
 
   constructor() { }
 
--- Module/modcom/src/app/tables/tables-home/tables-home.component.ts
+++ Module/modcom/src/app/tables/tables-home/tables-home.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tables-home.component.css']
 })
 export class TablesHomeComponent implements OnInit {
+  
 
   constructor() { }
 
