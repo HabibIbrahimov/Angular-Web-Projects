fcd95b33334f781f4e1e5e74bfa5aaa650c88c48
HabibIbrahimov <habibyi@code.edu.az>
2022-06-07 11:19:38 +0400

    feat:Children Route

--- Module/modcom/src/app/tables/tables-routing.module.ts
+++ Module/modcom/src/app/tables/tables-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 import { TablesHomeComponent } from './tables-home/tables-home.component';
 
 const routes: Routes = [
-  {path:'',component:TablesHomeComponent}
+  {path:'',component:TablesHomeComponent,
+   children:[
+     {path:'',component:BiographyComponent},
+     {path:'companies',component:CompaniesComponent},
+     {path:'partners',component:PartnersComponent}
+   ]
+   
+  }
 ];
 
 @NgModule({
--- Module/modcom/src/app/tables/tables.module.ts
+++ Module/modcom/src/app/tables/tables.module.ts
@@ -5,12 +5,18 @@ import { TablesRoutingModule } from './tables-routing.module';
 import { TablesHomeComponent } from './tables-home/tables-home.component';
 import { TableComponent } from './table/table.component';
 import { SharedModule } from '../shared/shared.module';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 
 @NgModule({
   declarations: [
     TablesHomeComponent,
-    TableComponent
+    TableComponent,
+    BiographyComponent,
+    CompaniesComponent,
+    PartnersComponent
   ],
   imports: [
     CommonModule,
