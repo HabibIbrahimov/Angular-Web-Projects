e809cf91b14f3b3976b47b89d448ff3f6f5a2870
HabibIbrahimov <habibyi@code.edu.az>
2022-06-12 18:52:09 +0400

    feat:Http Client

--- Random Picture API/photoapp/src/app/app.module.ts
+++ Random Picture API/photoapp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import{HttpClientModule}from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { DisplayphotoComponent } from './displayphoto/displayphoto.component';
@@ -10,7 +11,7 @@ import { DisplayphotoComponent } from './displayphoto/displayphoto.component';
     DisplayphotoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Random Picture API/photoapp/src/app/photo.service.ts
+++ Random Picture API/photoapp/src/app/photo.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,16 @@ import { Injectable } from '@angular/core';
 })
 export class PhotoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getPhotos()
+  {
+    return this.http.get('https://api.unsplash.com/photos/random',{
+      headers: {
+        Authorization: 'Client-ID 9x_Wxlq3YtU61D4xsJGJaqyTQ0_dAupvBdT0y1V5zrE'
+      }
+    }
+    );
+  }
+    
 }
